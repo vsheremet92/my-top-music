c3597d7f3d85461f986506308047ea4f192c7778
Valerii <vsheremet92@gmail.com>
2018-01-06 20:26:59 +0200

    MongoDB, collection 'my-top-music' and form submitting provided

--- client/components/view/widget.tsx
+++ client/components/view/widget.tsx
@@ -5,7 +5,14 @@ export class Widget extends React.Component<{
 }, {}> {
     render() {
         return(
-            <div>{this.props.text}</div>
+            <div>
+                {this.props.text}
+                <form action="/quotes" method="POST">
+                    <input type="text" placeholder="name" name="name" />
+                    <input type="text" placeholder="quote" name="quote" />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         )
     }
 }
--- server/source/index.ts
+++ server/source/index.ts
@@ -1,12 +1,42 @@
 import * as express from 'express'
-let app = express();
+import * as bodyParser from 'body-parser'
+import * as mongo from 'mongodb'
+
+const app = express();
+const MongoClient = mongo.MongoClient;
+const url = 'mongodb://localhost:27017';
+const dbName = 'my-top-music';
 
 app.use(express.static('../../../valerii/Projects/my-top-music/client/build'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res)=> {
     res.sendFile('index.html');
 });
 
-app.listen(5000, function () {
-    console.log('App listening');
-})
+app.post('/quotes', (req, res)=> {
+    console.log(`Received next data: name - ${req.body.name}, quote - ${req.body.quote}`);
+    res.redirect('/');
+});
+
+const findDocuments = function(db, callback) {
+    const collection = db.collection(dbName);
+    collection.find().toArray((err, res)=> {
+        console.log(res);
+    })
+    /*collection.insertMany([{a : 1}, {a : 2}, {a : 3}], (err, result)=> {
+        console.log("Inserted 3 documents into the collection");
+        callback(result);
+    });*/
+}
+
+MongoClient.connect(url, (err, client)=> {
+    console.log("Connected successfully to database");
+    const db = client.db(dbName);
+    findDocuments(db, ()=> {
+        client.close();
+    });
+    app.listen(5000, ()=> {
+        console.log('Server running on 5000');
+    });
+});
